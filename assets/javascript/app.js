9ca4b925bd647027143fec8c61ed84c0dec75dde
Bradley Knutson <bradley@bknutson.com>
2019-10-30 16:29:17 -0600

    Added functions for displaying and randomizing chosen question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,40 +1,65 @@
 var flashCardGame = {
     chosenQuestion: '',
     answersArray: [],
+    timer: false,
+    randomFlashCard: false,
     flashcards: {
         card1: {
             question: "Which code allows you to connect to an external javascript file?",
-            answer: "<script src=\"otherFile.js\"></script>",
-            wrong1: "<script src=otherFile.js></script>",
-            wrong2: "<script>\"otherFile.js\"</script>",
-            wrong3: "<script source=\"otherFile.js\"></script>"
+            answer: "&lt;script src=\"otherFile.js\"&gt;&lt;/script&gt;",
+            wrongAnswers: ["&lt;script src=otherFile.js&gt;&lt;/script&gt;", "&lt;script&gt;\"otherFile.js\"&lt;/script&gt;", "&lt;script source=\"otherFile.js\"&gt;&lt;/script&gt;"]
         },
         card2: {
             question: "How would you use the Javascript Query Selector to find an element with an ID of 'newId'?",
             answer: "document.querySelector(\"#newId\");",
-            wrong1: "document.query(\"#newId\");",
-            wrong2: "document.querySelector(\".newId\");",
-            wrong3: "document.query(#newId);"
+            wrongAnswers: ["document.query(\"#newId\");", "document.querySelector(\".newId\");", "document.query(#newId);"]
         },
+    },
+    startGame: function(){
+        flashCardGame.chooseQuestion();
+        flashCardGame.displayQuestion();
+        $('.answerChoice').on('click', function(){
+            if($(this).html() === flashCardGame.randomFlashCard.answer){
+                console.log('true');
+            }else{
+                console.log('false');
+            }
+        })
     },
     startTimer: function(){
         var secondsRemaining = 30;
-        var timer = setInterval(function(){
+        flashCardGame.timer = setInterval(function(){
             $('#timeRemaining').text(secondsRemaining);
             if(secondsRemaining === 0){
-                clearInterval(timer);
+                clearInterval(flashCardGame.timer);
             }else{
                 secondsRemaining--;
             }
        }, 1000);
     },
     chooseQuestion: function(){
-        var randomFlashCard = flashCardGame.flashcards[Object.keys(flashCardGame.flashcards)[Math.floor(Math.random() * Object.keys(flashCardGame.flashcards).length)]];
-        console.log(randomFlashCard);
+        flashCardGame.randomFlashCard = flashCardGame.flashcards[Object.keys(flashCardGame.flashcards)[Math.floor(Math.random() * Object.keys(flashCardGame.flashcards).length)]];
+        flashCardGame.chosenQuestion = flashCardGame.randomFlashCard.question;
+        var tempAnswers = [];
+        tempAnswers.push(flashCardGame.randomFlashCard.answer);
+        flashCardGame.randomFlashCard.wrongAnswers.forEach(answer => {
+            tempAnswers.push(answer);
+        });
+        for (let i = tempAnswers.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [tempAnswers[i], tempAnswers[j]] = [tempAnswers[j], tempAnswers[i]];
+        }
+        flashCardGame.answersArray = tempAnswers;
+            
     },
     displayQuestion: function(){
-
+        $('#question').text(flashCardGame.chosenQuestion);
+        flashCardGame.answersArray.forEach(answer => {
+            $('#answers').append("<pre value=true class=answerChoice>" + answer + "</pre>" ); 
+        });
     }
+
+    
 };
 
 //Set timer to 30 seconds and countdown once per second
@@ -45,4 +70,4 @@ var flashCardGame = {
 //In incorrect, notify user and show correct answer
 //After 10 seconds, automatically move on to next question.
 
-flashCardGame.chooseQuestion();
+flashCardGame.startGame();
